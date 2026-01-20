1758d406634e89424d6c66033787a104977f5951
rahuldohare007 <rahuldohare401@gmail.com>
2026-01-20 21:14:41 +0530

    Good

---
+++ C++ DSA/7.TUF/3.Arrays/2.Medium/003.MaximumSubarray.cpp
@@ -0,0 +1,33 @@
+// LeetCode: 53. Maximum Subarray
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+    int res = nums[0];
+    int total = 0;
+
+    for (int n : nums)
+    {
+        if (total < 0)
+        {
+            total = 0;
+        }
+
+        total += n;
+        res = max(res, total);
+    }
+
+    return res;
+}
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+int main()
+{
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout << "Maximum Subarray Sum: " << maxSubArray(nums) << endl;
+    
+    return 0;
+}
