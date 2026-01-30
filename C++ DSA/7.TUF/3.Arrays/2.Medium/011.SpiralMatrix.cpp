b5892ed05b61c09d37d95956d586d0bba7ec9960
rahuldohare007 <rahuldohare401@gmail.com>
2026-01-30 21:49:15 +0530

    Good

---
+++ C++ DSA/7.TUF/3.Arrays/2.Medium/011.SpiralMatrix.cpp
@@ -0,0 +1,53 @@
+// LeetCode: 54.Spiral Matrix
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+    vector<int> result;
+    if (matrix.empty()) return result;
+
+    int top = 0, bottom = matrix.size() - 1;
+    int left = 0, right = matrix[0].size() - 1;
+
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++)
+            result.push_back(matrix[top][i]);
+        top++;
+
+        for (int i = top; i <= bottom; i++)
+            result.push_back(matrix[i][right]);
+        right--;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--)
+                result.push_back(matrix[bottom][i]);
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                result.push_back(matrix[i][left]);
+            left++;
+        }
+    }
+    return result;
+}
+// Time Complexity: O(m*n)
+// Space Complexity: O(1)
+
+int main()
+{
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    vector<int> result = spiralOrder(matrix);
+    for (int num : result) {
+        cout << num << " ";
+    }
+    
+    return 0;
+}
