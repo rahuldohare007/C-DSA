f482db3df85040b7d62c83196fbb6eb5a9eebdf8
rahuldohare007 <rahuldohare401@gmail.com>
2026-03-12 13:06:39 +0530

    Good

---
+++ C++ DSA/7.TUF/4.Binary Search/1.BS on 1D Arrays/001.SearchXinSortedArray.cpp
@@ -0,0 +1,32 @@
+// LeetCode: 704. Binary Search
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+    cout << search(nums, target) << endl;
+
+    return 0;
+}
